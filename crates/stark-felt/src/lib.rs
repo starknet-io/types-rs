f0352146393cdd909948000c71e8300085459b2f
Federica <fedemoletta@hotmail.com>
2023-07-15 10:53:05 -0300

    Fix

--- crates/stark-felt/src/lib.rs
+++ crates/stark-felt/src/lib.rs
@@ -207,12 +207,12 @@ impl Felt {
 
     pub fn from_dec_str(dec_string: &str) -> Result<Self, FromStrError> {
         if dec_string.starts_with('-') {
-            UnsignedInteger::from_dec_str(dec_string)
-                .map(|x| Self(FieldElement::from(&x)))
-                .map_err(|_| FromStrError)
-        } else {
             UnsignedInteger::from_dec_str(dec_string.strip_prefix('-').unwrap())
                 .map(|x| Self(FieldElement::from(&x)).neg())
+                .map_err(|_| FromStrError)
+        } else {
+            UnsignedInteger::from_dec_str(dec_string)
+                .map(|x| Self(FieldElement::from(&x)))
                 .map_err(|_| FromStrError)
         }
     }
