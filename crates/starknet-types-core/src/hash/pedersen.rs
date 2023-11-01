529dd0d1ba373f670c54d509a28c83ff14c4cc9d
Pedro Fontana <fontana.pedro93@gmail.com>
2023-11-01 15:40:47 -0300

    implement Pedersen struct

--- crates/starknet-types-core/src/hash/pedersen.rs
+++ crates/starknet-types-core/src/hash/pedersen.rs
@@ -9,16 +9,10 @@ use super::traits::StarkHash;
 
 pub struct Pedersen;
 
-impl Pedersen {
-    pub fn new() -> Pedersen {
-        Pedersen
-    }
-}
-
 impl StarkHash for Pedersen {
-    /// Computes Pedersen hash using STARK curve on two elements, as defined
+    /// Computes the Pedersen hash of two Felts, as defined
     /// in <https://docs.starknet.io/documentation/architecture_and_concepts/Hashing/hash-functions/#pedersen_hash.>
-    fn hash(&self, felt_0: &Felt, felt_1: &Felt) -> Felt {
+    fn hash(felt_0: &Felt, felt_1: &Felt) -> Felt {
         let pedersen = PedersenLambdaworks::default();
 
         let hash = pedersen.hash(&felt_0.0, &felt_1.0);
@@ -26,9 +20,9 @@ impl StarkHash for Pedersen {
         Felt(hash)
     }
 
-    /// Computes Pedersen hash using STARK curve on an array of elements, as defined
+    /// Computes the Pedersen hash of an array of Felts, as defined
     /// in <https://docs.starknet.io/documentation/architecture_and_concepts/Hashing/hash-functions/#array_hashing.>
-    fn hash_array(&self, felts: &[Felt]) -> Felt {
+    fn hash_array(felts: &[Felt]) -> Felt {
         let pedersen = PedersenLambdaworks::default();
         let data_len =
             Felt::from_u128(u128::try_from(felts.len()).expect("Got 2^128 felts or more."))
@@ -47,7 +41,6 @@ mod tests {
 
     #[test]
     fn test_pedersen_hash() {
-        let pedersen = Pedersen::new();
         let x =
             Felt::from_hex("0x03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb")
                 .unwrap();
@@ -56,7 +49,7 @@ mod tests {
                 .unwrap();
 
         assert_eq!(
-            pedersen.hash(&x, &y),
+            Pedersen::hash(&x, &y),
             Felt::from_hex("0x030e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662")
                 .unwrap()
         );
@@ -64,13 +57,12 @@ mod tests {
 
     #[test]
     fn test_pedersen_hash_array() {
-        let pedersen = Pedersen::new();
         let a = Felt::from_hex("0xaa").unwrap();
         let b = Felt::from_hex("0xbb").unwrap();
         let c = Felt::from_hex("0xcc").unwrap();
         let expected =
             Felt::from_hex("0x10808e8929644950878c4f71326e47c6b584d9cfea2de0415daf8def0f5e89f")
                 .unwrap();
-        assert_eq!(pedersen.hash_array(&[a, b, c]), expected);
+        assert_eq!(Pedersen::hash_array(&[a, b, c]), expected);
     }
 }
--- crates/starknet-types-core/src/hash/traits.rs
+++ crates/starknet-types-core/src/hash/traits.rs
@@ -1,11 +1,10 @@
 use crate::felt::Felt;
 
 pub trait StarkHash {
-    /// Computes Pedersen hash using STARK curve on two elements, as defined
-    /// in <https://docs.starknet.io/documentation/architecture_and_concepts/Hashing/hash-functions/#pedersen_hash.>
-    fn hash(&self, felt_0: &Felt, felt_1: &Felt) -> Felt;
+    /// Computes the hash of two Felt
+    fn hash(felt_0: &Felt, felt_1: &Felt) -> Felt;
 
-    /// Computes Pedersen hash using STARK curve on an array of elements, as defined
-    /// in <https://docs.starknet.io/documentation/architecture_and_concepts/Hashing/hash-functions/#array_hashing.>
-    fn hash_array(&self, felts: &[Felt]) -> Felt;
+    /// Computes the hash of an array of Felts,
+    /// as defined in <https://docs.starknet.io/documentation/architecture_and_concepts/Hashing/hash-functions/#array_hashing.>
+    fn hash_array(felts: &[Felt]) -> Felt;
 }
