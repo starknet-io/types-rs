0b6b3046df4e3bbb66804cf156dbbe70aa6e0c7a
Damir Vodenicarevic <damipator@gmail.com>
2023-12-21 19:17:34 +0100

    Add parity-scale-codec serialization for Felts.
    Adds parity-scale-codec binary serialization and deserialization
    for Felts under the parity-scale-codec feature flag.

--- crates/starknet-types-core/src/felt.rs
+++ crates/starknet-types-core/src/felt.rs
@@ -391,6 +391,26 @@ impl<'a> Arbitrary<'a> for Felt {
     }
 }
 
+/// Allows transparent binary serialization of Felts with `parity_scale_codec`.
+#[cfg(feature = "parity-scale-codec")]
+impl parity_scale_codec::Encode for Felt {
+    fn encode_to<T: parity_scale_codec::Output + ?Sized>(&self, dest: &mut T) {
+        dest.write(&self.to_bytes_be());
+    }
+}
+
+/// Allows transparent binary deserialization of Felts with `parity_scale_codec`
+#[cfg(feature = "parity-scale-codec")]
+impl parity_scale_codec::Decode for Felt {
+    fn decode<I: parity_scale_codec::Input>(
+        input: &mut I,
+    ) -> Result<Self, parity_scale_codec::Error> {
+        let mut buf: [u8; 32] = [0; 32];
+        input.read(&mut buf)?;
+        Ok(Felt::from_bytes_be(&buf))
+    }
+}
+
 /// Defaults to [Felt::ZERO].
 impl Default for Felt {
     fn default() -> Self {
@@ -1731,5 +1751,28 @@ mod test {
         let one: Felt = true.into();
         assert_eq!(one, Felt::ONE);
         assert_eq!(zero, Felt::ZERO);
+    }
+
+    /// Tests proper serialization and deserialization of felts using `parity-scale-codec`.
+    #[test]
+    #[cfg(feature = "parity-scale-codec")]
+    fn parity_scale_codec_serialization() {
+        use parity_scale_codec::{Decode, Encode};
+
+        // use an endianness-asymetric number to test that byte order is correct in serialization
+        let initial_felt: Felt = Felt::from_hex("0xabcdef123").unwrap().try_into().unwrap();
+
+        // serialize the felt
+        let mut serialized_felt = Vec::new();
+        initial_felt.encode_to(&mut serialized_felt);
+
+        // deserialize the felt
+        let deserialized_felt = Felt::decode(&mut &serialized_felt[..]).unwrap();
+
+        // check that the deserialized felt is the same as the initial one
+        assert_eq!(
+            initial_felt, deserialized_felt,
+            "mismatch between original and deserialized felts"
+        );
     }
 }
