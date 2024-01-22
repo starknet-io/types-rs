bd6b6ee9fa2c2a6fc87ba20767703e9a6ebdc659
Nils <80390054+nils-mathieu@users.noreply.github.com>
2024-01-22 14:36:24 +0100

    Add support for the v0.6 of the Starknet API (#35)

---
+++ crates/starknet-types-rpc/src/v0_6_0/mod.rs
@@ -0,0 +1,16 @@
+//! v0.6.0 of the API.
+
+pub use starknet_types_core::felt::Felt;
+
+pub use crate::custom::{
+    BlockId, BroadcastedDeclareTxn, BroadcastedDeployAccountTxn, BroadcastedInvokeTxn,
+    SyncingStatus,
+};
+
+mod starknet_api_openrpc;
+mod starknet_trace_api_openrpc;
+mod starknet_write_api;
+
+pub use self::starknet_api_openrpc::*;
+pub use self::starknet_trace_api_openrpc::*;
+pub use self::starknet_write_api::*;
