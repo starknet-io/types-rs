21e7cb59fa5f116cdf069c1f345d1d5e5565eb18
Maciej Kamiński @ StarkWare <aundumla@gmail.com>
2024-04-23 16:03:05 +0200

    few more cases

--- fuzz/felt/fuzz_targets/add.rs
+++ fuzz/felt/fuzz_targets/add.rs
@@ -2,16 +2,17 @@
 use libfuzzer_sys::fuzz_target;
 use starknet_types_core::felt::Felt;
 
-fuzz_target!(|data: (Felt, Felt)| {
+fuzz_target!(|data: (Felt, Felt, Felt)| {
     let zero = Felt::ZERO;
-    let (a, b) = data;
+    let (a, b, c) = data;
+
     // Check a + 0 = a
-    assert_eq!(a + zero, a, "Zero addition failed");
-    assert_eq!(b + zero, b, "Zero addition failed");
+    assert_eq!(a + zero, a, "zero addition failed");
+    assert_eq!(b + zero, b, "zero addition failed");
 
     // Check a + (-a) = 0
-    assert_eq!(a + (-a), zero, "Unary addition failed");
-    assert_eq!(b + (-b), zero, "Unary addition failed");
+    assert_eq!(a + (-a), zero, "unary addition failed");
+    assert_eq!(b + (-b), zero, "unary addition failed");
 
     // Check a + b = a - (-b)
     assert_eq!(a + b, a - (-b), "addition failed");
@@ -21,5 +22,14 @@ fuzz_target!(|data: (Felt, Felt)| {
     assert_eq!(b + b, b - (-b), "addition failed");
 
     // Check a + a = 2 * a
-    assert_eq!(a + a, Felt::TWO * a, "Doubling failed");
+    assert_eq!(a + a, Felt::TWO * a, "doubling failed");
+
+    // Check a + b = b + a
+    assert_eq!(a + b, b + a, "commutativity failed");
+
+    // Check (a + b) + c = a + (b + c)
+    assert_eq!((a + b) + c, a + (b + c), "associativity failed");
+
+    // Check a + max = a - 1
+    assert_eq!(a + Felt::MAX, a - Felt::ONE, "overflow failed");
 });
