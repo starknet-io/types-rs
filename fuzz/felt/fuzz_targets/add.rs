a825c648d0004d845664ace43b894c407afcc689
Maciej Kamiński <aundumla@gmail.com>
2024-04-24 12:36:50 +0200

    refactoring

--- fuzz/felt/fuzz_targets/add.rs
+++ fuzz/felt/fuzz_targets/add.rs
@@ -4,12 +4,22 @@ use starknet_types_core::felt::Felt;
 
 fuzz_target!(|data: (Felt, Felt, Felt)| {
     let zero = Felt::ZERO;
+    let one = one;
+    let max = max;
     let (a, b, c) = data;
 
     // Check a + 0 = a
     assert_eq!(a + zero, a, "zero addition failed");
     assert_eq!(b + zero, b, "zero addition failed");
 
+    // Check a - 0 = a
+    assert_eq!(a - zero, a, "zero subtraction failed");
+    assert_eq!(b - zero, b, "zero subtraction failed");
+
+    // Check a - a = 0
+    assert_eq!(a - a, zero, "unary subtraction failed");
+    assert_eq!(b - b, zero, "unary subtraction failed");
+    
     // Check a + (-a) = 0
     assert_eq!(a + (-a), zero, "unary addition failed");
     assert_eq!(b + (-b), zero, "unary addition failed");
@@ -31,5 +41,8 @@ fuzz_target!(|data: (Felt, Felt, Felt)| {
     assert_eq!((a + b) + c, a + (b + c), "associativity failed");
 
     // Check a + max = a - 1
-    assert_eq!(a + Felt::MAX, a - Felt::ONE, "overflow failed");
+    assert_eq!(a + max, a - one, "overflow failed");
+    
+    // Check 0 - a = max - a + 1
+    assert_eq!(zero - a, max - a + one, "overflow failed");
 });
