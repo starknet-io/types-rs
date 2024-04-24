e9f9b422338ced7617f7d63eeb3ef086ac455243
Maciej Kamiński @ StarkWare <aundumla@gmail.com>
2024-04-24 12:00:34 +0200

    mul, sub fuzzers

---
+++ fuzz/felt/fuzz_targets/mul.rs
@@ -0,0 +1,32 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use starknet_types_core::felt::Felt;
+
+fuzz_target!(|data: (Felt, Felt)| {
+    let zero = Felt::ZERO;
+    let one = Felt::ONE;
+    let (a, b) = data;
+
+    // Check a * 1 = a
+    assert_eq!(a * one, a, "multiplicative identity failed");
+    assert_eq!(b * one, b, "multiplicative identity failed");
+
+    // Check 1 * a = a
+    assert_eq!(one * a, a, "multiplicative identity failed");
+    assert_eq!(one * b, b, "multiplicative identity failed");
+
+    // Check a * 0 = 0
+    assert_eq!(a * zero, zero, "zero product failed");
+    assert_eq!(b * zero, zero, "zero product failed");
+
+    // Check 0 * a = a
+    assert_eq!(zero * a, zero, "zero product failed");
+    assert_eq!(zero * b, zero, "zero product failed");
+
+    // Check a * max = max - a + 1
+    assert_eq!(a * Felt::MAX, Felt::MAX - a + 1, "overflow failed");
+
+    // Check a * b = b * a
+    assert_eq!(a * b, b * a, "commutativity failed");
+
+});
