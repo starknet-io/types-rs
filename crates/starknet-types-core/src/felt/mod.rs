38e58e575345d549c7489f6a576a7227bed6b7eb
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2025-06-24 15:25:35 -0300

    chore: move zeroize and parity_scale_codec in their own files

--- crates/starknet-types-core/src/felt/mod.rs
+++ crates/starknet-types-core/src/felt/mod.rs
@@ -1,7 +1,11 @@
 #[cfg(test)]
 mod felt_arbitrary;
 mod non_zero;
+#[cfg(feature = "parity-scale-codec")]
+mod parity_scale_codec;
 mod primitive_conversions;
+#[cfg(feature = "zeroize")]
+mod zeroize;
 
 pub use non_zero::NonZeroFelt;
 
@@ -420,26 +424,6 @@ impl Arbitrary<'_> for Felt {
     #[inline]
     fn size_hint(depth: usize) -> (usize, Option<usize>) {
         <[u64; 4]>::size_hint(depth)
-    }
-}
-
-/// Allows transparent binary serialization of Felts with `parity_scale_codec`.
-#[cfg(feature = "parity-scale-codec")]
-impl parity_scale_codec::Encode for Felt {
-    fn encode_to<T: parity_scale_codec::Output + ?Sized>(&self, dest: &mut T) {
-        dest.write(&self.to_bytes_be());
-    }
-}
-
-/// Allows transparent binary deserialization of Felts with `parity_scale_codec`
-#[cfg(feature = "parity-scale-codec")]
-impl parity_scale_codec::Decode for Felt {
-    fn decode<I: parity_scale_codec::Input>(
-        input: &mut I,
-    ) -> Result<Self, parity_scale_codec::Error> {
-        let mut buf: [u8; 32] = [0; 32];
-        input.read(&mut buf)?;
-        Ok(Felt::from_bytes_be(&buf))
     }
 }
 
@@ -967,13 +951,6 @@ mod errors {
             "failed to create Felt from string".fmt(f)
         }
     }
-}
-
-#[cfg(feature = "zeroize")]
-mod zeroing {
-    use super::Felt;
-
-    impl zeroize::DefaultIsZeroes for Felt {}
 }
 
 #[cfg(test)]
@@ -1631,26 +1608,6 @@ mod test {
     }
 
     /// Tests proper serialization and deserialization of felts using `parity-scale-codec`.
-    #[test]
-    #[cfg(feature = "parity-scale-codec")]
-    fn parity_scale_codec_serialization() {
-        use parity_scale_codec::{Decode, Encode};
-
-        // use an endianness-asymetric number to test that byte order is correct in serialization
-        let initial_felt = Felt::from_hex("0xabcdef123").unwrap();
-
-        // serialize the felt
-        let serialized_felt = initial_felt.encode();
-
-        // deserialize the felt
-        let deserialized_felt = Felt::decode(&mut &serialized_felt[..]).unwrap();
-
-        // check that the deserialized felt is the same as the initial one
-        assert_eq!(
-            initial_felt, deserialized_felt,
-            "mismatch between original and deserialized felts"
-        );
-    }
     #[cfg(feature = "papyrus-serialization")]
     #[test]
     fn hash_serde() {
@@ -1677,16 +1634,6 @@ mod test {
             let d = Felt::deserialize(&mut reader).unwrap();
             assert_eq!(Felt::from_bytes_be(&bytes), d);
         }
-    }
-
-    #[cfg(feature = "zeroize")]
-    #[test]
-    fn zeroing_felt() {
-        use zeroize::Zeroize;
-
-        let mut felt = Felt::from_hex_unchecked("0x01");
-        felt.zeroize();
-        assert_eq!(felt, Felt::ZERO);
     }
 
     #[test]
