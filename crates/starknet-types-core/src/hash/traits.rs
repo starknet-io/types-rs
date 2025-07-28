1e304dd6cf14a92d5fb1bb4a8ccf9116ae3552b9
Isaac Izard <loopyloopy811@gmail.com>
2025-07-28 20:34:23 +0200

    Update crates/starknet-types-core/src/hash/traits.rs
    Co-authored-by: 0xLucqs <70894690+0xLucqs@users.noreply.github.com>

--- crates/starknet-types-core/src/hash/traits.rs
+++ crates/starknet-types-core/src/hash/traits.rs
@@ -5,7 +5,7 @@ pub trait StarkHash {
     fn hash(felt_0: &Felt, felt_1: &Felt) -> Felt;
 
     /// Computes the hash of an array of Felts,
-    /// as defined in <https://docs.starknet.io/architecture-and-concepts/cryptography/#array_hashing>
+    /// as defined in <https://docs.starknet.io/architecture/cryptography/#array_hashing>
     fn hash_array(felts: &[Felt]) -> Felt;
 
     fn hash_single(felt: &Felt) -> Felt;
