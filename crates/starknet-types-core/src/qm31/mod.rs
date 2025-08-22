295267791eb98c8826c2e7f8c0e7e41dbc57bd72
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-08-22 17:49:36 -0300

    Improve documentation

--- crates/starknet-types-core/src/qm31/mod.rs
+++ crates/starknet-types-core/src/qm31/mod.rs
@@ -5,10 +5,25 @@ use lambdaworks_math::field::{
 
 use crate::felt::Felt;
 
+/// A value in the Degree-4 (quadruple) extension of the Mersenne 31 field.
+///
+/// Each QM31 value is represented by two values in the Degree-2 (complex)
+/// extension, and each of these is represented with two values in the base
+/// field. Thus, a QM31 is represented by four M31 coordinates.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct QM31(pub FieldElement<Degree4ExtensionField>);
 
 impl QM31 {
+    /// Packs the [QM31] into a [Felt].
+    ///
+    /// Stores the four M31 coordinates in the first 144 bits of a Felt. Each
+    /// coordinate takes 36 bits, and the resulting felt is equal to:
+    /// `C1 + C2 << 36 + C3 << 72 + C4 << 108`
+    ///
+    /// Why the stride between coordinates is 36 instead of 31? In Stwo, Felt
+    /// elements are stored in memory as 28 M31s, each representing 9 bits
+    /// (that representation is efficient for multiplication). 36 is the first
+    /// multiple of 9 that is greater than 31.
     pub fn pack_into_felt(&self) -> Felt {
         // Take CM31 coordinates from QM31.
         let [a, b] = self.0.value();
@@ -26,14 +41,18 @@ impl QM31 {
         Felt::from_bytes_le(&felt_bytes)
     }
 
+    /// Unpacks a [QM31] from the [Felt]
+    ///
+    /// See the method [QM31::pack_into_felt] for a detailed explanation on the
+    /// packing format.
     pub fn unpack_from_felt(felt: &Felt) -> QM31 {
         const MASK_36: u64 = (1 << 36) - 1;
         const MASK_8: u64 = (1 << 8) - 1;
 
         let digits = felt.to_le_digits();
 
-        // As we are packing the QM31 in the first 144 bits, the remaining bits
-        // must be zero.
+        // The QM31 is packed in the first 144 bits,
+        // the remaining bits must be zero.
         if digits[3] != 0 || digits[2] >= 1 << 16 {
             panic!("a")
         }
@@ -44,8 +63,8 @@ impl QM31 {
         let c3 = (digits[1] >> 8) & MASK_36;
         let c4 = (digits[1] >> 44) + (digits[2] << 20);
 
-        // Even though we use 36 bits for each coordinate, the maximum value is
-        // still the field prime.
+        // Even though we use 36 bits for each coordinate,
+        // the maximum value is still the field prime.
         for c in [c1, c2, c3, c4] {
             if c >= MERSENNE_31_PRIME_FIELD_ORDER as u64 {
                 panic!("b")
